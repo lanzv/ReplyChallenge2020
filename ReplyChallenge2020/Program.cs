791c65e548624bd74ced4236af9cc3287f6a5d52
Vojtěch Lanz <vojtech.lanz@gmail.com>
2020-03-12 19:26:40 +0100

    RunEZ, first solution

--- ReplyChallenge2020/Program.cs
+++ ReplyChallenge2020/Program.cs
@@ -4,18 +4,16 @@ namespace ReplyChallenge2020
 {
     public class Program
     {
-        public static readonly string INPUT_FILE = "a.txt";
-        public static readonly string OUTPUT_FILE = "output_1.txt";
+        public static readonly string INPUT_FILE = "f.txt";
+        public static readonly string OUTPUT_FILE = "output_6.txt";
 
         static void Main(string[] args)
         {
             ReplyChallenge RC = new ReplyChallenge();
 
             Reader reader = new Reader(INPUT_FILE);
-           
-            Console.ReadLine();
-
-            RC.Run();
+            RC.EZRun();
+            RC.GenerateOutputFile();
         }
     }
 }
